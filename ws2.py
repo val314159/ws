8982ac775783e39d9ef619302a3331bd84c7b562
Joel Ward <jmward@gmail.com>
2020-10-14 21:10:11 -0700

    update

--- ws2.py
+++ ws2.py
@@ -1,37 +1,55 @@
-from geventwebsocket import WebSocketError
-from geventwebsocket import WebSocketServer
-import bottle, json
+from gevent import monkey ; monkey.patch_all()
+from bottle import request, abort, default_app
 
-@bottle.route('/')
-def handle_index():
-    return '''
-<!DOCTYPE html>
-<html>
-<head>
-<script type="text/javascript">
-var ws = new WebSocket("ws://localhost:8080/websocket");
-ws.onopen = function() {
-  ws.send("Hello, world");
-};
-ws.onmessage = function (evt) {
-  alert(evt.data);
-};
-</script>
-</head>
-</html>'''
+app = default_app()
 
-@bottle.route('/websocket')
+@app.route('/websocket')
 def handle_websocket():
-    wsock = bottle.request.environ.get('wsgi.websocket')
+    wsock = request.environ.get('wsgi.websocket')
     if not wsock:
-        bottle.abort(400, 'Expected WebSocket request.')
-
-    try:
-        while True:
+        abort(400, 'Expected WebSocket request.')
+    wsock.send("helloooooooo")
+    while True:
+        try:
             message = wsock.receive()
             wsock.send("Your message was: %r" % message)
-    except WebSocketError:
-        pass # break
+        except WebSocketError:
+            print("ERR")
+            break
+        pass
     pass
 
-WebSocketServer(("", 8080), bottle.app()).serve_forever()
+@app.route('/static/<path:path>')
+def statics(path):
+    return static_file(path, 'static')
+
+@app.route('/tmpl/<path:path>')
+def statics(path):
+    return static_file(path, 'tmpl')
+
+@app.route('/')
+def index():
+    return '''
+<!DOCTYPE html>
+    <html>
+    <head>
+    <script type="text/javascript">
+    WS=(L,x)=>`ws${L.protocol.substr(4)}//${L.host}`+x;
+    var ws = new WebSocket(WS(location,"/websocket"));
+    ws.onopen = function() {
+    ws.send("Hello, world");
+    };
+    ws.onmessage = function (evt) {
+    alert(evt.data);
+    };
+    </script>
+    </head>
+    </html>
+'''
+
+from gevent.pywsgi import WSGIServer
+from geventwebsocket import WebSocketError
+from geventwebsocket.handler import WebSocketHandler
+server = WSGIServer(("0.0.0.0", 8081), app,
+                    handler_class=WebSocketHandler)
+server.serve_forever()
